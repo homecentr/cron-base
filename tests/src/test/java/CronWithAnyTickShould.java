3647b7297d81c94695db9ad0fcca3326d45b0261
LH <l.holota@outlook.com>
2020-05-25 15:01:38 +0200

    test: PUID/PGID of the cron tick execution added (#6)

---
+++ tests/src/test/java/CronWithAnyTickShould.java
@@ -0,0 +1,51 @@
+import helpers.CronDockerTagResolver;
+import io.homecentr.testcontainers.containers.GenericContainerEx;
+import io.homecentr.testcontainers.containers.wait.strategy.WaitEx;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import static io.homecentr.testcontainers.WaitLoop.waitFor;
+import static org.junit.Assert.assertEquals;
+
+public class CronWithAnyTickShould {
+    private static GenericContainerEx _cronContainer;
+
+    @BeforeClass
+    public static void before() {
+        _cronContainer = new GenericContainerEx<>(new CronDockerTagResolver())
+                .withEnv("PUID", "7088")
+                .withEnv("PGID", "7099")
+                .withEnv("CRON_SCHEDULE", "* * * * *")
+                .withRelativeFileSystemBind(Paths.get("..", "example", "long-success").toString(), "/cron")
+                .waitingFor(WaitEx.forS6OverlayStart());
+
+        _cronContainer.start();
+    }
+
+    @AfterClass
+    public static void after() {
+        _cronContainer.close();
+    }
+
+    @Test
+    public void executeCronTickScriptWithPassedPuid() throws Exception {
+        waitFor(Duration.ofSeconds(80), () -> _cronContainer.getLogsAnalyzer().contains("Execution started"));
+
+        int puid = _cronContainer.getProcessUid("ash /usr/sbin/cron-tick");
+
+        assertEquals(7088, puid);
+    }
+
+    @Test
+    public void executeCronTickScriptWithPassedPgid() throws Exception {
+        waitFor(Duration.ofSeconds(80), () -> _cronContainer.getLogsAnalyzer().contains("Execution started"));
+
+        int pgid = _cronContainer.getProcessGid("ash /usr/sbin/cron-tick");
+
+        assertEquals(7099, pgid);
+    }
+}
