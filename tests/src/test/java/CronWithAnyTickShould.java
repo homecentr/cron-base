e91589621dbba44cd13dabec87b058dc4b970e18
Lukas Holota <l.holota@outlook.com>
2020-05-25 14:53:15 +0200

    fix

--- tests/src/test/java/CronWithAnyTickShould.java
+++ tests/src/test/java/CronWithAnyTickShould.java
@@ -1,21 +1,15 @@
 import helpers.CronDockerTagResolver;
-import helpers.PushGatewayContainerFactory;
 import io.homecentr.testcontainers.containers.GenericContainerEx;
-import io.homecentr.testcontainers.containers.HttpResponse;
 import io.homecentr.testcontainers.containers.wait.strategy.WaitEx;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.testcontainers.containers.Container;
-import org.testcontainers.containers.Network;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.Duration;
 
 import static io.homecentr.testcontainers.WaitLoop.waitFor;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class CronWithAnyTickShould {
     private static GenericContainerEx _cronContainer;
